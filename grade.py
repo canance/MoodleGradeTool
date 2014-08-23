6fbb8926192240f2abd3064edab96d67583f70b4
Phillip Wall <tsyshadow@gmail.com>
2014-08-23 19:02:44 -0400

    1. Performed Code Cleanup 2. Switch to using a regex to extract information from file names

--- grade.py
+++ grade.py
@@ -5,97 +5,140 @@
 # Assumptions: Submissions were mass-downloaded from Moodle and unzipped.
 ##########################################################################
 
-import os; 
-import sys;
+##########################################################################
+# Modified by:  Phillip Wall
+# Date:        22 August 2014
+# Note:        Removed all the unnecessary semi-colons at the end of the lines
+##########################################################################
+
+import os
+import sys
+
+import subprocess
+import re
 
 def main():
-	if len(sys.argv) < 2: 
-		path = raw_input("Please enter the absolute path (zip/folder): ");
-	else:
-		path = str(sys.argv[1]);
-	#end if
-	
-
-
-	#TODO Finish adding zip support
-	z = len(path) - 4;
-	if '.zip' is path[z:]:
-		os.system( "mkdir " + path[0:z]);
-		
-	files = os.listdir(path);
-	
-	for f in files:
-		
-		#ignore dot files. 
-		if '.' is f[0:1]:
-			continue;
-
-	
-		# Moodle renames all submissions so they cannot be compiled as is. Luckily
-		# it does include the original filename so we can paritition the string
-		# and extract the original filename from it.  From there we can cp the file 
-		# in order to compile and run.
-
-		student = f.partition('_')[0];
-		tempFile = path + "/" + f.partition('assignsubmission_file_')[2];
-		realFile = path + "/" + f;
-		className = tempFile.replace(".java", "").replace(path + "/","");
-
-		classDeclaration = "public class " + className;
-		
-		fHandle = open(realFile);
-		for line in fHandle:
-			if classDeclaration in line:
-				break;
-			if "package " in line:
-				package = line.replace("package ", "").partition(';')[0];
-				#1. create a new directory for the package
-				#2. Move the class file to the package directory
-				
-				cmd = ( "mkdir \"" + path + "/" + package + 
-					"\"; mv \"" + path + "/" + className + 
-					".class\" \"" + path + "/" + package + "/" + 
-					className + ".class\""  
-				 );
-
-				print cmd;
-				os.system(cmd);
-				className = package + "." + className;
-		#end for line in f
-		fHandle.close();
+    if len(sys.argv) < 2:
+        path = raw_input("Please enter the absolute path (zip/folder): ")
+    else:
+        path = str(sys.argv[1])
+    #end if
 
 
 
-		print "##################################\n";
-		print student;
-		print realFile + "\n\n";
+    #TODO Finish adding zip support
+    # Fixed comparison to leverage negative indexes -Phillip Wall
+    if '.zip' == path[-4:]:
+        os.system( "mkdir " + path)
+
+    files = os.listdir(path)
+
+    # Following regex will match strings in the format of "Student Name_assignsubmission_file_ClassName.java"
+    # It will extract the student name and class name using capture groups
+    # We compile it here to save time in the loop -Phillip Wall
+    regex = re.compile(r"""
+        (           #Start of first Capture group
+            [^_]+   #Matches everything until the first underscore
+        )
+        _[^e]+e_    #Consumes everything after the underscore until it gets to e and then consumes e_
+        (           #Start of second capture group
+            [^.]+   #Matches everything until a period
+        )""", re.VERBOSE)
 
 
-		cmds = [];
-		cmds.append( "cp \"" + realFile + "\" \"" + tempFile + "\"" );
-		cmds.append( "javac \"" + tempFile + "\"" );
-		cmds.append( "cd \"" + path + "\" && " + "java \"" + className + "\"" );
+    for f in files:
 
-		for c in cmds:
-			os.system(c);
-
+        #ignore dot files.
+        #Fixed: unnecessary slicing operation -Phillip Wall
+        if '.' == f[0]:
+            continue
 
 
-		raw_input("Press enter to continue...");
-		
-		cmds = [];
-		cmds.append( "clear" );
-		cmds.append( "rm -f " + tempFile );
-		cmds.append( "vim \"" + realFile + "\"");
+        # Moodle renames all submissions so they cannot be compiled as is. Luckily
+        # it does include the original filename so we can paritition the string
+        # and extract the original filename from it.  From there we can cp the file 
+        # in order to compile and run.
 
-		for c in cmds:
-			os.system(c);
+        ##Next section commented out by Phillip Wall
+        # student = f.partition('_')[0]
+        # tempFile = path + "/" + f.partition('assignsubmission_file_')[2]
+        # realFile = path + "/" + f
+        # className = tempFile.replace(".java", "").replace(path + "/","")
 
-	#end for f in files
+        ## Start author Phillip Wall
+        m = regex.search(f) #Attempt to match the filename
+
+        if not m:
+            continue #If there is not a match this filename does not match the expected format, skip it.
+
+        student = m.group(1) #Get the student name from the match
+        className = m.group(2) #Get the className from the match
+        tempFile = "{}/{}.java".format(path, className)
+        realFile = "{}/{}".format(path, f)
+
+        ## End author Phillip Wall
+
+        classDeclaration = "public class " + className
+
+        #Use context manager for handling file objects (no need to explicitly close the file) -Phillip Wall
+        with open(realFile) as fHandle:
+            for line in fHandle:
+                if classDeclaration in line:
+                    break
+                if "package " in line:
+                    #Strip trailing ';' with strip method -Phillip Wall
+                    package = line.replace("package ", "").strip(';')
+                    #1. create a new directory for the package
+                    #2. Move the class file to the package directory
+
+                    #Use the string format method to create the command to move the java file -Phillip Wall
+                    cmd = 'mkdir "{path}/{package}"; mv "{path}/{name}.java" "{path}/{package}/{name}.java"'.format(
+                        path=path, name=className, package=package)
+
+                    # cmd = ( "mkdir \"" + path + "/" + package +
+                    #         "\"; mv \"" + path + "/" + className +
+                    #         ".class\" \"" + path + "/" + package + "/" +
+                    #         className + ".class\""
+                    # )
+
+                    print cmd
+                    os.system(cmd)
+                    className = package + "." + className
+            #end for line in f
+
+
+
+        print "##################################\n"
+        print student
+        print realFile + "\n\n"
+
+
+        
+        cmds = []
+        cmds.append( "cp \"" + realFile + "\" \"" + tempFile + "\"" )
+        cmds.append( "javac \"" + tempFile + "\"" )
+        cmds.append( "cd \"" + path + "\" && " + "java \"" + className + "\"" )
+
+        for c in cmds:
+            os.system(c)
+
+
+
+        raw_input("Press enter to continue...")
+
+        cmds = []
+        cmds.append( "clear" )
+        cmds.append( "rm -f " + tempFile )
+        cmds.append( "vim \"" + realFile + "\"")
+
+        for c in cmds:
+            os.system(c)
+
+        #end for f in files
 #end main
 
 
 
 if __name__ == "__main__":
-	main();
+    main()
 #end if
