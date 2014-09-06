3a353630f426723c89ac37cb7d26201da27b1f3c
Cory Nance <canance@coastal.edu>
2014-09-05 21:22:48 -0400

    Redesigned  filemanger to be a module instead of class.  Simplified paths variable to be src and dest file name instead of paths.

--- filemanager.py
+++ filemanager.py
@@ -4,48 +4,42 @@ import os
 import shutil
 import uuid
 
+history = {}
 
-class FileManager(object):
+def copy(paths, stuname):
     """
-    Static class to manage files that need to be copied and later removed.
+    Copies the files to the students working path.
+    :param paths: a list of tuples consisting of (src_file, dst_file)
+    :param stuName: name of student
+    :return: a key to be used when the resources need to be cleaned (removed)
     """
+    spaths = []
+    for s, d in paths:
+        try:
+            dst = "%s/%s" % (stuname, d)
+            shutil.copy(s, dst)
+            spaths.append((s, d))
+        except IOError, e:
+            print str(e)
 
-    history = {}
+    record = {'name': stuname, 'paths': spaths}
+    key = str(uuid.uuid4())
+    history[key] = record
+    return key
 
-    @staticmethod
-    def copy(paths, stuname):
-        """
-        Copies the files specified in .
-        :param paths: a list of tuples consisting of (src, dst)
-        :param stuName: name of student
-        :return: a key to be to clean up files after tests are run
-        """
 
-        spaths = []
-        for src, dst in paths:
-            try:
-                shutil.copy(src, dst)
-                spaths.append((src, dst))
-            except IOError, e:
-                print str(e)
+def clean(key):
+    """
+    Deletes the files associated with specified key
+    :param key: a key that was returned when calling copy()
+    """
+    record = history.pop(key)
+    stuname = record['name']
+    paths = record['paths']
 
-        record = {'name': stuname, 'paths': spaths}
-        key = str(uuid.uuid4())
-        FileManager.history[key] = record
-        return key
-
-    @staticmethod
-    def clean(key):
-        """
-        Deletes the files associated with specified key
-        :param key: a key that was returned when calling copy()
-        """
-        #stuname = FileManager.history[key]['name']
-        paths = FileManager.history[key]['paths']
-
-        for s, d in paths:
-            if os.path.isfile(d):
-                print "removing file %s." % d
-                os.remove(d)
-            else:
-                shutil.rmtree(d) #directory
+    for s, d in paths:
+        dst = "%s/%s" % (stuname, d)
+        if os.path.isfile(dst):
+            os.remove(dst)
+        else:
+            shutil.rmtree(dst)
