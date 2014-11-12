8dec449b9b203303181080f34d0e5d71cf4ecb11
Phillip Wall <tsyshadow@gmail.com>
2014-11-12 18:55:49 -0500

    Cleanup

--- qmoodlegrade.py
+++ qmoodlegrade.py
@@ -9,9 +9,7 @@ from PySide.QtCore import QThread
 
 def main():
     qt.mainthread = QThread.currentThread()
-    startview()
-    while qt.mainview.rootObject() is None:
-        pass
+    qt.initialize_view()
     dispatch_thread = QThread()
     qt.maindispatch = QTDispatcher(qt.mainview)
     qt.qapp.lastWindowClosed.connect(dispatch_thread.quit)
@@ -21,25 +19,6 @@ def main():
     res = qt.qapp.exec_()
     dispatch_thread.wait()
     sys.exit(res)
-
-
-def startview():
-    qt.initialize_view()
-
-def start_dispatcher():
-    qt.maindispatch = QTDispatcher(qt.mainview)
-    cur = QThread.currentThread()
-    if not (qt.mainthread is None or cur is qt.mainthread):
-        #qt.qapp.lastWindowClosed.connect(cur.quit())
-        cur.exec_()
-
-class disthread(QThread):
-
-    def __init__(self, **kwargs):
-        super(disthread, self).__init__(**kwargs)
-
-    def run(self, *args, **kwargs):
-        start_dispatcher()
 
 if __name__ == "__main__":
     main()
